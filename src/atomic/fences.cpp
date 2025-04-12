e6336d8efe45712f38d5fd1e6b93c9040d2da230
Krishna Addepalli <coolkrishna31@gmail.com>
2025-04-12 09:54:45 +0200

    Add nonatomic variable sync with atomic variable code to fences

--- src/atomic/fences.cpp
+++ src/atomic/fences.cpp
@@ -24,6 +24,23 @@ void read_y_then_x() {
     }
 }
 
+auto a = false;         // a is now a plain nonatomic variable.
+std::atomic<bool> b;
+std::atomic<int> c;
+
+void write_a_then_b() {
+    a = true;       // <--- (6)  Store to a before the fence
+    std::atomic_thread_fence(std::memory_order_release);
+    b.store(true, std::memory_order_relaxed);   // <--- (7) store to b after the fence
+}
+
+void read_a_then_b() {
+    while (!b.load(std::memory_order_relaxed)) {}   // <--- (8) wait until you see the write from (7)
+    std::atomic_thread_fence(std::memory_order_acquire);
+    if (a)          // <--- (9) This will read the value written by #6
+        ++ c;
+}
+
 /**
  *
  * Fences
@@ -45,14 +62,55 @@ void read_y_then_x() {
  * sees the result of a release operation, the release operation synchronizes-with the acquire fence. Although the fence
  * synchronization depends on the values read or written by operations before or after the fence, it's important to note
  * that the synchronization point is the fence itself.
+ *
+ * However, the real benefit to using atomic operations to enforce ordering is that they can enforce an ordering on
+ * nonatomic operations and thus avoid the undefined behavior of a data race.
+ *
+ * The fences still provide an enforced ordering of the store to a (6) and the store to b (7) and the load from b (8) and
+ * the load from a (9), and there's still a happens-before relationship between the store to a and the load from a, so the
+ * assert (10) still won't fire. The store to (7) and load from (8) b still have to be atomic; otherwise there would be
+ * a data race on b, but the fences enforce an ordering on the operations on a, once the reading thread has seen the stored
+ * value of b. This enforced ordering means that there's no data race on  a, even though it's modified by one thread and
+ * read by another.
+ *
+ * Ordering of nonatomic operations through the use of atomic operations is where the sequenced-before part of happens-before
+ * becomes so important. If a nonatomic operation is sequenced before an atomic operation, and that atomic operation
+ * happens-before an operation in another thread, the nonatomic operation also happens-before that operation in the other
+ * thread.
+ *
+ * The lock() operation is a loop on flag.test_and_set() using std::memory_order_acquire ordering, and the unlock() is a
+ * call to flag.clear() with std::memory_order_release ordering. When the first thread calls lock(), the flag is initially
+ * clear, so the first call to test_and_set() will set the flag and return false, indicating that this thread now has the
+ * lock, and terminating the loop. The thread is then free to modify any data protected by the mutex. Any other thread
+ * that calls lock() at this time will find the flag already set and will be blocked in the test_and_set() loop.
+ * When the thread with the lock has finished modifying the protected data, it calls unlock(), which calls flag.clear()
+ * with std::memory_order_release semantics. This then synchronizes-with a subsequent call to flat.test_and_set() from
+ * an invocation of lock() on another thread, because this call has std::memory_order_acquire semantics. Because the
+ * modification of the protected data is necessarily sequenced before the unlock() call, this modification happens-before
+ * the unlock() and thus happens-before the subsequent lock() call from the second thread (because of the synchronizes-with
+ * relationship between the unlock() and the lock()) and happens-before any accesses to that data from this second thread
+ * once it has acquired the lock.
+ *
+ * Although other mutex implementations will have different internal operations, the basic principle is the same: lock()
+ * is an acquire operation on an internal memory location, and unlock() is a release operation on that same memory
+ * location.
  */
 int main() {
     x=false;
     y=false;
     z=0;
-    std::thread a (write_x_then_y);
-    std::thread b(read_y_then_x);
-    a.join();
-    b.join();
+    std::thread xt (write_x_then_y);
+    std::thread yt(read_y_then_x);
+    xt.join();
+    yt.join();
     assert(z.load() !=0);
+
+    a = false;
+    b = false;
+    c = 0;
+    std::thread at(write_a_then_b);
+    std::thread bt(read_a_then_b);
+    at.join();
+    bt.join();
+    assert(c.load() != 0);
 }
