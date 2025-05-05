4232cda2977004ef5ac689d19d5931e619f306b8
Krishna Addepalli <coolkrishna31@gmail.com>
2025-05-05 05:48:10 +0200

    Add code to test both sequential and parallel quick sorts

--- src/synchronizing_concurrent_operations/test_quick_sort.cpp
+++ src/synchronizing_concurrent_operations/test_quick_sort.cpp
@@ -2,13 +2,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <functional>
+#include <chrono>
 
 #include "synchronizing_concurrent_operations/sequential_quick_sort.h"
 #include "synchronizing_concurrent_operations/parallel_quick_sort.h"
 
 auto generate_random_numbers(const size_t count)-> std::list<int> {
     std::function<std::list<int>(std::list<int>, size_t)> go = [&](std::list<int> state, const size_t curr) -> std::list<int> {
-        if (curr == count) {
+        if (curr >= count) {
             return state;
         }
         state.push_back(std::rand());
@@ -19,10 +20,32 @@ auto generate_random_numbers(const size_t count)-> std::list<int> {
 };
 
 int main() {
-    const auto list = generate_random_numbers(100);
+    auto list = generate_random_numbers(1000);
+    auto list_copy = list;
 
-    for (const int v : list) {
-        std::cout << v << std::endl;
-    }
+    const auto start = std::chrono::high_resolution_clock::now();
+    const auto res = sequential_quick_sort(std::move(list));
+    const auto stop = std::chrono::high_resolution_clock::now();
+
+    std::cout << "sequential_quick_sort took "
+            << std::chrono::duration<double>(stop-start).count()
+            << "seconds" << std::endl;
+
+    // for (const int v : res) {
+    //     std::cout << v << std::endl;
+    // }
+
+    const auto start1 = std::chrono::high_resolution_clock::now();
+    const auto res1 = parallel_quick_sort(std::move(list_copy));
+    const auto stop1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "parallel_quick_sort took "
+            << std::chrono::duration<double>(stop1-start1).count()
+            << "seconds" << std::endl;
+
+    // for (const int v : res1) {
+    //     std::cout << v << std::endl;
+    // }
+
     return 0;
 }
