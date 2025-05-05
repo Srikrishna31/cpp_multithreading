8e4f288c9f787b639f9e64935e76ba641a1f1d59
Krishna Addepalli <coolkrishna31@gmail.com>
2025-05-05 05:57:13 +0200

    Add code for spawn_task which can be an alternative for std::async

--- src/synchronizing_concurrent_operations/test_quick_sort.cpp
+++ src/synchronizing_concurrent_operations/test_quick_sort.cpp
@@ -19,6 +19,25 @@ auto generate_random_numbers(const size_t count)-> std::list<int> {
     return go(std::list<int>(), 0);
 };
 
+/**
+ * A sample implementation of a task spawning function which can be used instead of std::async.
+ * @tparam F
+ * @tparam A
+ * @param f
+ * @param a
+ * @return
+ */
+template<typename F, typename A>
+auto spawn_task(F&& f, A&& a) -> std::future<std::result_of_t<F(A&&)> > {
+    using result_type = std::result_of_t<F(A&&)>;
+    std::packaged_task<result_type(A&&)> task(std::forward<F>(f));
+    std::future<result_type> future = task.get_future();
+    std::thread t (std::move(task), std::forward<A>(a));
+    t.detach();
+
+    return future;
+}
+
 int main() {
     auto list = generate_random_numbers(1000);
     auto list_copy = list;
