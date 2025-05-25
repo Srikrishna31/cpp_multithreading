6bc9d2c800afbc6416b207a040ac0d1145b630fb
Krishna Addepalli <coolkrishna31@gmail.com>
2025-05-25 20:37:25 +0200

    Add code for call_once, function local static singleton

--- src/modern_cpp_concurrency_udemy/lazy_initialization.cpp
+++ src/modern_cpp_concurrency_udemy/lazy_initialization.cpp
@@ -6,6 +6,11 @@
 
 class Test {
     public:
+
+    Test() {
+        std::cout << "Test()" << std::endl;
+    }
+
     void operator()() const {
         std::cout << "Function call operator called!" << std::endl;
     }
@@ -13,6 +18,9 @@ class Test {
 
 Test *ptest = nullptr;      // Variable to be lazy initialized.
 std::mutex m;
+
+Test* ptest2 = nullptr;
+std::once_flag flag;    // The flag stores synchronization data.
 
 /**
  * Thread-safe lazy initialization
@@ -33,9 +41,96 @@ void process() {
         ptest = new Test;
     }
     lock.unlock();
+    ptest->operator()();
+}
+
+/**
+ * With this simple implementation of double-checked locking, there is still a possibility of a race-condition
+ *      * The initialization of ptest involves several operations
+ *          -> Allocate enough memory to store a Test object.
+ *          -> Construct a Test object in the memory
+ *          -> Store the address in ptest
+ *      * C++ allows these to be performed in a different order, e.g.
+ *          -> Allocate enough memory to store a Test object.
+ *          -> Store the address in ptest.
+ *          -> Construct a Test object in the memory
+ *
+ *      * Undefined behaviour
+ *          -> Thread A checks ptest and locks the mutex
+ *          -> Thread A allocates the memory and assigns to ptest
+ *          -> However, it has not yet called the constructor
+ *          -> Thread B checks ptest and it is not null
+ *          -> Thread B doesnot lock the mutex
+ *          -> Thread B jumps out of the if statement
+ *          -> Thread B calls a member function of an uninitialized object: Undefined behavior
+ *
+ *      * std::call_once
+ *          -> One way to solve the above undefined behavior is to use std::call_once()
+ *              . A given function is only called once
+ *              . It is done in one thread
+ *              . The thread cannot be interrupted until the function call completes.
+ *          -> We use it with a global instance of std::once_flag
+ *          -> We pass the instance and the function to std::call_once()
+ */
+auto double_checked_process() {
+    if (!ptest) {
+        std::lock_guard<std::mutex> lock(m);
+        if (!ptest) {
+            ptest = new Test;
+        }
+    }
+
+    ptest->operator()();
+}
+
+/**
+ * Double-checked locking with std::call_once
+ *      . Threadsafe
+ *      . Less overhead than a mutex.
+ *
+ * Double-checked locking in C++17
+ *     * C++17 defines the order of initialization
+ *          . Allocate enough memory to store a Test object.
+ *          . Construct a Test object in the memory
+ *          . Store the address in ptest
+ *     * Double-checked locking no longer causes a data race.
+ */
+auto process_with_call_once() {
+    std::call_once(flag, []() {
+        ptest = new Test;
+    });
+    ptest->operator()();
+}
+
+/**
+ *
+ * Meyer's singleton pattern (function local static)
+ *
+ * Conclusion:
+ *  * Four ways to do thread-safe lazy initialization
+ *      . Naive use of a mutex
+ *      . Use std::call_once()
+ *      . Double-checked locking with a C++17 compiler or later
+ *      . Meyers singleton with static local variable.
+ *
+ *  * Recommendations
+ *      . Use Meyers singleton, if ptest is not required outside the function.
+ *      . Otherwise use std::call_once()
+ */
+void process_singleton() {
+    static Test ptest;      // Variable which is lazily initialized
+
     ptest();
 }
 
 auto main() -> int {
-    process();
+    // process();
+
+    std::thread t(process_with_call_once);
+    std::thread t2(process_with_call_once);
+    t.join();
+    t2.join();
+
+    process_singleton();
+    return 0;
 }
